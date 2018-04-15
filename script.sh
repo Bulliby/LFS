881a7c4cce299605aa73aedeaaf386cf614bd56e
Guillaume Wells <wellsguillaume@gmail.com>
2018-04-15 13:43:13 +0200

    feat(master): get package from md5sums list

--- script.sh
+++ script.sh
@@ -15,3 +15,23 @@ mv wget-list md5sums pkg
 cd pkg
 
 pkgs=`cat md5sums`
+i=1
+index=0
+
+for pkg in $pkgs
+do
+    if [ `expr $i % 2` -eq 0 ]
+    then
+        ret[$index]="$pkg"
+        index=`expr $index + 1`
+    fi
+    i=`expr $i + 1`
+done
+
+#Print parsed package
+#i=0
+#while [ "$i" -lt $index ]
+#do
+#    echo "${ret[$i]}"
+#    i=`expr $i + 1`
+#done
